e25726add9610740897a800c3dbdb03142a109a7
fab <fabiantoby80@gmail.com>
2022-12-01 02:45:39 -0800

    task8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+1 #!/usr/bin/python3
+  2 str = "Python is an interpreted, interactive, object-oriented programming\
+  3  language that combines remarkable power with very clear syntax"
+  4 print(f"{str[39:66]} with {str[0:7]}")
+  5 #print(str)
+
